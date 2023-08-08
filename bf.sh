5f3887d093888b6bcbf1ab6c1af4471c416f6870
TryBin03 <try2029138116@163.com>
2023-08-08 14:26:56 +0800

    支持配置程序路径

--- bf.sh
+++ bf.sh
@@ -1,5 +1,7 @@
 # 需要备份的数据库名称
 dbName=$1
+# 程序路径
+path=/backup
 # MySQL用户
 MySQLUser=MySQL 
 # MySQL用户密码
@@ -8,7 +10,7 @@ MySQLPwd=MySQL
 zipPwd=password
 
 # 设定 文件夹
-backUpFolder="/backup/data/$dbName"  #定时保存打包数据库的文件 的位置
+backUpFolder="$path/data/$dbName"  #定时保存打包数据库的文件 的位置
 date_now=`date +%Y_%m_%d_%H%M`
 backFileName=$dbName_$date_now
 
@@ -26,4 +28,4 @@ zip -rP $zipPwd  $backFileName.zip $backFileName
 # 移除临时文件
 rm -rf $backFileName
 
-NODE_ENV=$backUpFolder@$backFileName@$dbName node /backup/start.js # 此程序位置
+NODE_ENV=$backUpFolder@$backFileName@$dbName node $path/start.js # 此程序位置
